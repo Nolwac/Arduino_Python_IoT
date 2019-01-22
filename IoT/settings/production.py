3453380e46f0b3852646b6a3fe6168a145120a11
Nolwac <livinusanayo96@gmail.com>
2019-01-22 17:25:37 +0100

    Add static files folder and make some changes on the settings package

--- IoT/settings/production.py
+++ IoT/settings/production.py
@@ -1,16 +1,6 @@
-import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .base import *
 
 import dj_database_url
 # dj_url = dj_database_url.config()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 DATABASES['default']=dj_database_url.config()
 # DATABASES['default']['CONN_MAX_AGE']=500
